3697fd86e446c450287cb9ac3d989e77c7066686
marcolino386 <marcolinoderos@hotmail.com>
2015-01-20 06:55:21 -0200

    create account

--- criarconta.py
+++ criarconta.py
@@ -1,16 +1,24 @@
 from tkinter import *
 import sqlite3
 
-def criar_conta():
-    con = sqlite3.connect('logindata.sqlite')
-    cursor = con.cursor()
-    cursor.execute("""
-                   INSERT INTO ids(id, senha, email)
-                   VALUES (?,?,?)""",
-                   (user_new.get(), password_new.get(), email_new.get()))
-    con.commit()
 
-     
+def conta_criar():
+    try:
+        con = sqlite3.connect('login.sqlite')
+        cursor = con.cursor()
+        cursor.execute("""
+                       INSERT INTO ids(id, senha, email)
+                       VALUES (?,?,?)""",
+                       (user_new.get(),
+                        password_new.get(),
+                        email_new.get()))
+        con.commit()
+        stats.set('sua conta foi criada com sucesso')
+        con.close()
+    except Exception as err:
+        stats.set(str(err))
+        con.close()
+      
 
 
 root = Tk()
@@ -26,6 +34,11 @@ new_password = Entry(root, textvariable=password_new).pack()
 
 Label(root, text="digite seu endereço de email:").pack()
 email_new = StringVar()
-new_email = Entry(root, textvariable=email_new).pack() 
-Button(root, text='Criar conta', command= criar_conta).pack()
- 
+new_email = Entry(root, textvariable=email_new).pack()
+
+stats = StringVar()
+Label(root, textvariable=stats).pack()
+Button(root, text='Criar conta', command= conta_criar).pack()
+Button(root, text="sair", command=root.destroy).pack()
+
+root.mainloop() 
