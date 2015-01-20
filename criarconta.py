f559c9cd3b2b17814467aa86cd89c913824a82f5
marcolino386 <marcolinoderos@hotmail.com>
2015-01-20 02:42:23 -0200

    COMPLETE

--- criarconta.py
+++ criarconta.py
@@ -1,6 +1,16 @@
 from tkinter import *
+import sqlite3
 
 def criar_conta():
+    con = sqlite3.connect('logindata.sqlite')
+    cursor = con.cursor()
+    cursor.execute("""
+                   INSERT INTO ids(id, senha, email)
+                   VALUES (?,?,?)""",
+                   (user_new.get(), password_new.get(), email_new.get()))
+    con.commit()
+
+     
 
 
 root = Tk()
@@ -13,5 +23,9 @@ Label(root, text="digite sua senha:").pack()
 
 password_new = StringVar()
 new_password = Entry(root, textvariable=password_new).pack()
+
+Label(root, text="digite seu endereço de email:").pack()
+email_new = StringVar()
+new_email = Entry(root, textvariable=email_new).pack() 
 Button(root, text='Criar conta', command= criar_conta).pack()
  
