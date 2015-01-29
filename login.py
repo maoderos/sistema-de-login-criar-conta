b9a882673f76cf317bb7602359f773a8359dbcc9
Marcos <marcolinoderos@hotmail.com>
2015-01-29 19:05:38 -0200

    now with class!

--- login.py
+++ login.py
@@ -1,50 +1,46 @@
 ''' interface do submit - usuario/senha'''
-from tkinter import *
+import tkinter
 import sqlite3
 
-def conta_criar():
-    import criarconta
+class Login(tkinter.Tk):
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.initialize()
 
-def login():
-    try:
-     con = sqlite3.connect('login.sqlite')
-     cursor = con.cursor()
-     cursor.execute('SELECT senha FROM ids WHERE id = "%s"' % usuario.get())
-     dados = cursor.fetchall()
-     dados_str = str(dados)
-    
-     (nada, senhas, nad) = dados_str.strip().split("'")
-     if senha.get() == senhas:
-         incorrect.set('login concluido!')
-     else:
-         incorrect.set('seu ID ou senha estao incorretos')
-     con.close()
-    except:
-         incorrect.set("seu ID ou senha estao incorretos")
-         con.close()
-       
+    def initialize(self):
+        self.usuario = tkinter.StringVar()
+        self.senha = tkinter.StringVar()
+        self.labl1 = tkinter.Label(self, text="Por favor digite seu login e senha").pack()
+        self.labl2 = tkinter.Label(self, text="Login:")
+        self.entry1 = tkinter.Entry(self, textvariable=self.usuario).pack()
+        self.labl3 = tkinter.Label(self, text='senha:').pack()
+        self.entry2 = tkinter.Entry(self, textvariable=self.senha).pack()
+        self.check = tkinter.StringVar()
+        self.labl4 = tkinter.Label(self, textvariable=self.check).pack()
+        self.button = tkinter.Button(self, text='entrar', command=self.fazer_login).pack()
+        #self.button2 = tkinter.Button(self, text='Criar conta', """commmand=conta_criar""").pack(side=RIGHT)
 
+    def fazer_login(self):
+        try:
+            self.con = sqlite3.connect('login.sqlite')
+            self.cursor = self.con.cursor()
+            self.cursor.execute('SELECT senha FROM ids WHERE id = "%s"' % self.usuario.get())
+            self.dados = self.cursor.fetchall()
+            self.dados_str = str(self.dados)
 
- 
-appli = Tk()
-appli.title("i dont know yet")
-Label(appli, text = "Por favor digite seu login e senha").pack()
-Label(appli, text = "Login:").pack()
+            (self.nda, self.senhas, self,nad) = self.dados_str.strip().split("'")
+            print(self.senhas)
+            """if self.senha.get() == self.senhas:
+                print(legal)"""
+        except Exception as err:
+            print(err)
+        
 
-usuario = StringVar()
-usuario_entry = Entry(appli, textvariable = usuario)
-usuario_entry.pack()
+app = Login()
+app.title('Login')
 
-
-Label(appli, text = "senha:").pack()
-senha = StringVar()
-senha_entry = Entry(appli, textvariable = senha)
-senha_entry.pack()
-
-incorrect = StringVar()
-Label(appli, textvariable = incorrect).pack()
-Button(appli, text='entrar', command=login ).pack(side=LEFT)
-#Button(appli, text='crie uma conta', command=conta_criar).pack(side=RIGHT)
+app.mainloop()
 
 
-appli.mainloop()
+
+
